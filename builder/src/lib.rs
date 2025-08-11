9c63f671ca9259476cb1c9f7a04e31a0cb0e0df6
felix <Wu-Felix@outlook.com>
2025-08-11 16:59:50 +0800

    feat(builder): 03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Error};
+use syn::{punctuated::Punctuated, DeriveInput, Error};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -13,14 +13,45 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn parse(input: &DeriveInput) -> Result<proc_macro2::TokenStream, Error> {
     let ident = &input.ident;
     let builder_name = quote::format_ident!("{ident}Builder");
+    let fields = extract_named_fields(input)?;
+    let fields_named = fields
+        .iter()
+        .filter_map(|f| f.ident.as_ref().map(|ident| (ident, &f.ty)))
+        .collect::<Vec<_>>();
+
+    let (idents, tys): (Vec<_>, Vec<_>) = fields_named.iter().cloned().unzip();
+    let functions = quote! {
+        impl #builder_name{
+            #(fn #idents(&mut self,#idents:#tys)->&mut Self{
+                self.#idents=std::option::Option::Some(#idents);
+                self
+            })*
+        }
+    };
     Ok(quote! {
-        struct #builder_name{}
+        struct #builder_name{
+            #(#idents:std::option::Option<#tys>),*
+        }
         impl #ident{
             fn builder()->#builder_name{
                 #builder_name{
-
+                    #(#idents:std::option::Option::None),*
                 }
             }
         }
+        #functions
     })
+}
+fn extract_named_fields(
+    input: &DeriveInput,
+) -> Result<&Punctuated<syn::Field, syn::Token![,]>, Error> {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = &input.data
+    {
+        Ok(named)
+    } else {
+        Err(Error::new_spanned(input, "没有匹配到结构体成员"))
+    }
 }
